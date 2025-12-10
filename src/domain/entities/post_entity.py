e2f307bb7f2975f60c820972a37d427fa47f7645
Henry Vilani <henryvilanimansur@gmail.com>
2025-12-10 10:13:15 -0300

    docs(post entity): add documentation to post entity

--- src/domain/entities/post_entity.py
+++ src/domain/entities/post_entity.py
@@ -5,18 +5,31 @@ from typing import List, Optional
 
 @dataclass()
 class Post:
+    """
+    Domain entity representing a Post.
+    
+    :param id: Id of the post.
+    :type id: str
+    :param user_id: User id associated with the post.
+    :type user_id: str
+    :param title: Title of the post.
+    :type title: str
+    :param content: Content of the post.
+    :type content: str
+    :param tags: List of tags of the post.
+    :type tags: List[str]
+    :param created_at: Datetime of post creation.
+    :type created_at: datetime
+    """    
 
     id: str
     user_id: str
-
     channel_id: Optional[str]
 
     # TODO: add multiple post types
 
     title: str
     content: str
-
     tags: List[str]
-
     created_at: datetime
 
