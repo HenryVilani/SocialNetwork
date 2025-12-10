6bdf13c113222741589005f78d6d1609f1746265
Henry Vilani <henryvilanimansur@gmail.com>
2025-12-10 11:16:25 -0300

    docs(use case): add documentation to auth use casae

--- src/application/use_cases/auth/decode_token_use_case.py
+++ src/application/use_cases/auth/decode_token_use_case.py
@@ -5,9 +5,23 @@ from src.domain.repositories.auth_repository import IAuthRepository
 
 @define
 class DecodeTokenUseCase:
+    """
+    Application use case for decode token to AuthUser.
+    
+    :param auth_repository: Auth repository abstraction.
+    :type auth_repository: IAuthRepository
+    """
 
     auth_repository: IAuthRepository
 
     def execute(self, token: str) -> AuthUser:
+        """
+        Decode token to basic user information (AuthUser)
+        
+        :param token: Token used to decode.
+        :type token: str
+        :return: basic user information
+        :rtype: AuthUser
+        """
 
         return self.auth_repository.decode_token(token)
--- src/application/use_cases/auth/login_use_case.py
+++ src/application/use_cases/auth/login_use_case.py
@@ -9,16 +9,32 @@ from src.domain.value_object.password_value_object import Password
 
 @define
 class LoginUserUseCase:
+    """
+    Application use case for login a user.
+    
+    :param auth_repository: Auth repository abstraction.
+    :type auth_repository: IAuthRepository
+    :param user_repository: User repository abstraction.
+    :type use_repository: IUserRepository
+    """
 
     auth_repository: IAuthRepository
     user_repository: IUserRepository
 
     def execute(self, email: str, password: str) -> str:
+        """
+        Login user with email and password.
+        
+        :param email: Email used to login user.
+        :type email: str
+        :param password: Passowrd used to login user.
+        :type password: str
+        :return: If login correctly return token string
+        :rtype: str
+        :raises WrongCredentialsError: If email or password is wrong.
+        """
 
         user = self.user_repository.find_by_email(Email(email))
-    
-        print("password: ", user.password)
-        print("Password: ", Password(password).value)
 
         if Password.verify(user.password, password):
             return self.auth_repository.generate_token(AuthUser(
